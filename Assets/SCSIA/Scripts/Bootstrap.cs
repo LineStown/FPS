f31b3464b6993e875e84082c08ec77280065a784
LineStown <tymur.bludov@outlook.com>
2025-07-12 10:39:37 +0300

    added level. added player controller

---
+++ Assets/SCSIA/Scripts/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using Assets.SCSIA.Scripts.Player;
+using UnityEngine;
+
+namespace Assets.SCSIA.Scripts
+{
+    internal class Bootstrap : MonoBehaviour
+    {
+        [SerializeField] PlayerController _playerController;
+
+        private void Awake()
+        {
+            _playerController.Initialize();
+        }
+    }
+}
